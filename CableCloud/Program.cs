2e019380e6074fb7f7474e7c4a9a80d06d08b67d
Tomasz <01133475@pw.edu.pl>
2020-04-10 01:35:18 +0200

    Początki chmury

--- CableCloud/Program.cs
+++ CableCloud/Program.cs
@@ -4,9 +4,9 @@ namespace CableCloud
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello my friend am your cable cloud");
         }
     }
 }
