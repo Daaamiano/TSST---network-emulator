c99d0d6eea595f2d4d83ea05b7fc210ee10ec0ca
Daniel Pisarek <d.pisarek@stud.elka.pw.edu.pl>
2020-04-11 00:18:48 +0200

    Raczkowanie hosta

--- Host/Host.cs
+++ Host/Host.cs
@@ -7,6 +7,61 @@ namespace Host
 {
     class Host
     {
+        private IpAddress hostSourceAddress;
+        private IpAddress hostSourcePort;
+        private IpAddress destinationHostPort;
+        private IpAddress cableCloudAddress;
+        private int cableCloudPort;
 
+        private Socket sendingToCableCloudSocket;
+        private Socket receivingFromCableCloudSocket; 
+
+        public Host() 
+        {
+            Console.Title = "Host";
+        }
+
+        public void Start()
+        {
+            //sendingToCableCloudSocket = new Socket(cableCloudAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            //ConnectToCableCloud();
+        }
+
+        private void ConnectToCableCloud()
+        {
+
+        }
+
+        public void Listen()
+        {
+            
+        }
+
+        public void Read(IAsyncResult ar)
+        {
+
+        }
+
+        public bool Send(string message) 
+        {
+            try
+            {
+                Console.WriteLine("Choose which host you want to send the package to: "); // wybieramy numer hosta - np. H1? czy po prostu docelowy numer portu hosta?
+                destinationHostPort = int.Parse(Console.ReadLine());
+                sendingToCableCloudSocket = new Socket((new IPEndPoint(cableCloudAddress, cableCloudPort)).AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                sendingToCableCloudSocket.Connect(new IPEndPoint(cableCloudAddress, cableCloudPort));
+                sendingToCableCloudSocket.Send(new Package(hostSourceAddress, hostSourcePort, destinationHostPort, message).toBytes());
+                //messageQueue.Enqueue(Logger.Log("Sent to host with port: " + destinationHostPort + ": " + message, LogType.INFO));
+                //sendingToCableCloudSocket.Close();
+            }
+            catch (Exception e)
+            {
+                //messageQueue.Enqueue(Logger.Log(e.Message, LogType.ERROR));
+               // if (Sender != null) sendingToCableCloudSocket.Close();
+                return false;
+            }
+            return true;
+            
+        }
     }  
 }
