c646fd62e9eaced6825a2bf002e916230e682985
Daniel Pisarek <d.pisarek@stud.elka.pw.edu.pl>
2020-04-11 21:40:10 +0200

    Asynchroniczny host - po raczkowaniu

--- Host/Host.cs
+++ Host/Host.cs
@@ -1,67 +1,138 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
+
 
 namespace Host
 {
+    public class ObjectState
+    {
+        public Socket workSocket = null;
+        public const int bufferSize = 256;
+        public byte[] buffer = new byte[bufferSize];
+        public StringBuilder sb = new StringBuilder();
+    } 
+    
     class Host
     {
-        private IpAddress hostSourceAddress;
-        private IpAddress hostSourcePort;
-        private IpAddress destinationHostPort;
-        private IpAddress cableCloudAddress;
-        private int cableCloudPort;
+        private const int cableCloudPort = 5001;
+        private static IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        private static ManualResetEvent connectCompleted = new ManualResetEvent(false);
+        private static ManualResetEvent sendCompleted = new ManualResetEvent(false);
+        private static ManualResetEvent receiveCompleted = new ManualResetEvent(false);
+        private static string response = String.Empty;
 
-        private Socket sendingToCableCloudSocket;
-        private Socket receivingFromCableCloudSocket; 
-
-        public Host() 
+        public Host()
         {
             Console.Title = "Host";
+            StartHost();
+            Console.ReadLine();
         }
 
-        public void Start()
-        {
-            //sendingToCableCloudSocket = new Socket(cableCloudAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            //ConnectToCableCloud();
-        }
-
-        private void ConnectToCableCloud()
-        {
-
-        }
-
-        public void Listen()
-        {
-            
-        }
-
-        public void Read(IAsyncResult ar)
-        {
-
-        }
-
-        public bool Send(string message) 
+        public static void StartHost()
         {
             try
             {
-                Console.WriteLine("Choose which host you want to send the package to: "); // wybieramy numer hosta - np. H1? czy po prostu docelowy numer portu hosta?
-                destinationHostPort = int.Parse(Console.ReadLine());
-                sendingToCableCloudSocket = new Socket((new IPEndPoint(cableCloudAddress, cableCloudPort)).AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-                sendingToCableCloudSocket.Connect(new IPEndPoint(cableCloudAddress, cableCloudPort));
-                sendingToCableCloudSocket.Send(new Package(hostSourceAddress, hostSourcePort, destinationHostPort, message).toBytes());
-                //messageQueue.Enqueue(Logger.Log("Sent to host with port: " + destinationHostPort + ": " + message, LogType.INFO));
-                //sendingToCableCloudSocket.Close();
+                Socket hostSocket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                hostSocket.BeginConnect(new IPEndPoint(ipAddress, cableCloudPort), 
+                    new AsyncCallback(ConnectionCallBack), hostSocket);
+                Send(hostSocket, $"Test taki o se {DateTime.Now} <EOF>");
+                sendCompleted.WaitOne();
+
+                Receive(hostSocket);
+                receiveCompleted.WaitOne();
+                Console.WriteLine($"Response received {response}");
+                hostSocket.Shutdown(SocketShutdown.Both);
+                hostSocket.Close();
+
             }
             catch (Exception e)
             {
-                //messageQueue.Enqueue(Logger.Log(e.Message, LogType.ERROR));
-               // if (Sender != null) sendingToCableCloudSocket.Close();
-                return false;
+                Console.WriteLine(e);
             }
-            return true;
-            
         }
-    }  
+
+        private static void Receive(Socket hostSocket)
+        {
+            try 
+            {
+                ObjectState state = new ObjectState();
+                state.workSocket = hostSocket;
+                hostSocket.BeginReceive(state.buffer, 0, ObjectState.bufferSize, 0, 
+                    new AsyncCallback(ReceiveCallback), state);
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private static void ReceiveCallback(IAsyncResult ar)
+        {
+            ObjectState state = (ObjectState) ar.AsyncState;
+            var hostSocket = state.workSocket;
+            int byteRead = hostSocket.EndReceive(ar);
+            if (byteRead > 0)
+            {
+                state.sb.Append(Encoding.ASCII.GetString(state.buffer,0,byteRead));
+                hostSocket.BeginReceive(state.buffer, 0, ObjectState.bufferSize, 0, 
+                    new AsyncCallback(ReceiveCallback), state);
+            }
+            else
+            {
+                if(state.sb.Length > 1)
+                {
+                    response = state.sb.ToString();
+                }
+                
+                receiveCompleted.Set();
+            }
+        }
+
+        private static void Send(Socket hostSocket, string data)
+        {
+            byte[] byteData = Encoding.ASCII.GetBytes(data);
+            hostSocket.BeginSend(byteData, 0, byteData.Length, 0, 
+                new AsyncCallback(SendCallback), hostSocket);
+        }
+
+        private static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket hostSocket = (Socket) ar.AsyncState;
+                int byteSent = hostSocket.EndSend(ar);
+                Console.WriteLine($"Sent: {byteSent} bytes to Cable Cloud");
+                sendCompleted.Set();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private static void ConnectionCallBack(IAsyncResult ar)
+        {
+            try
+            {
+                Socket hostSocket = (Socket) ar.AsyncState;
+                hostSocket.EndConnect(ar);
+                Console.WriteLine($"Socket connection : {hostSocket.RemoteEndPoint.ToString()}");
+                connectCompleted.Set();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+    
 }
+
+// w jaki sposob pakujemy pakiet? - json? czy string z portem docelowym i wiadomoscia oddzielny np. srednikiem - potem cablecloud sobie odczyta to
+// SocketFlags - moze do tworzenia oakietu (rozne ifnromacje tam schowac) ????
+// The program '[19332] Host.dll' has exited with code -1 (0xffffffff). - o co chodzi???
+// czy .net core 3.1 jest na pewno git? - czy zabiera jakies funkcje; czy nie lepsze framework jak komandos sugerowal ?
--- Host/Program.cs
+++ Host/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -7,54 +8,10 @@ namespace Host
 {
     class Program
     {
-
-        private static Socket _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         static void Main(string[] args)
         {
-            Console.Title = "Client";
-            LoopConnect();
-            SendLoop();
-            Console.ReadLine();
+            Host host = new Host();   
         }
 
-
-        private static void SendLoop()
-        {
-            while (true)
-            {
-                Console.Write("Enter a request: ");
-                string req = Console.ReadLine();
-                byte[] buffer = Encoding.ASCII.GetBytes(req);
-                _clientSocket.Send(buffer);
-
-                //byte[] receivedBuf = new byte[1];
-                //int rec = _clientSocket.Receive(receivedBuf);
-                //byte[] data = new byte[rec];
-                //Array.Copy(receivedBuf, data, rec);
-                //Console.WriteLine("Received: " + Encoding.ASCII.GetString(data));
-            }
-        }
-        private static void LoopConnect()
-        {
-            int attempts = 0;
-
-            while (!_clientSocket.Connected)
-            {
-                try
-                {
-                    attempts++;
-                    _clientSocket.Connect(IPAddress.Loopback, 100);
-                    
-                }
-                catch (SocketException)
-                {
-                    Console.Clear();
-                    Console.WriteLine("Connect attempts: " + attempts.ToString());
-                    Console.WriteLine("IPAddress.Loopback: " + IPAddress.Loopback);
-                }
-            }
-            Console.Clear();
-            Console.WriteLine("Connected");
-        }
     }
 }
