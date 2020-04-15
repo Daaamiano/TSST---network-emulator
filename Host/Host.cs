2bbba187344e7fdaa55814d291b2d1d0f5503a7b
Daniel Pisarek <d.pisarek@stud.elka.pw.edu.pl>
2020-04-16 00:38:02 +0200

    Dzialające polączenie z cable cloud

--- Host/Host.cs
+++ Host/Host.cs
@@ -32,6 +32,68 @@ namespace Host
 
         public Host(string filePath)
         {
+            LoadPropertiesFromFile(filePath);
+            Console.Title = hostName;
+            StartHost();
+        }
+
+        public static void StartHost()
+        {
+            Socket hostSocket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            hostSocket.BeginConnect(new IPEndPoint(ipAddress, cableCloudPort),
+            new AsyncCallback(ConnectionCallBack), hostSocket);
+
+
+            bool correctChoice = false;
+            while(true)
+            {
+                Console.WriteLine("You are host");
+                Console.WriteLine("Write '1'  if you want to send the message to another host ");
+
+                int decision = int.Parse(Console.ReadLine());
+                if (decision == 1)
+                {
+                    try
+                    {
+                        correctChoice = true;
+                        Console.WriteLine("Choose which host you want to send the package to");
+                        Console.WriteLine(@"To do this, write 'H1' or 'H2' or 'H3' or 'H4' -> host 1 is 'H1' etc.");
+                        string nameDestinationHost = Console.ReadLine();
+                        if (nameDestinationHost == "H1")
+                            destinationPort = h1Port;
+                        else if (nameDestinationHost == "H2")
+                            destinationPort = h2Port;
+                        else if (nameDestinationHost == "H3")
+                            destinationPort = h3Port;
+                        else if (nameDestinationHost == "H4")
+                            destinationPort = h4Port;
+
+                        Send(hostSocket, $"Test taki o se {DateTime.Now} port docelowy to {destinationPort} <EOF>");
+                        sendCompleted.WaitOne();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }        
+                else
+                {
+                    correctChoice = false;
+                    Console.WriteLine("You wrote something other than '1' ");
+                    Console.WriteLine("Please try again: ");
+                }
+
+                Receive(hostSocket);
+                receiveCompleted.WaitOne();
+                Console.WriteLine($"Response received {response}");
+                hostSocket.Shutdown(SocketShutdown.Both);
+                hostSocket.Close();
+            }
+            
+        }
+
+        private void LoadPropertiesFromFile(string filePath)
+        {
             string[] lines = System.IO.File.ReadAllLines(filePath);
             hostName = lines[1];
             sourcePort = int.Parse(lines[2]);
@@ -62,62 +124,6 @@ namespace Host
                 h2Port = int.Parse(lines[4]);
                 h3Port = int.Parse(lines[5]);
             }
-            Console.Title = hostName;
-            StartHost();
-        }
-
-        public static void StartHost()
-        {
-            Socket hostSocket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            hostSocket.BeginConnect(new IPEndPoint(ipAddress, cableCloudPort),
-            new AsyncCallback(ConnectionCallBack), hostSocket);
-
-            Receive(hostSocket);
-            receiveCompleted.WaitOne();
-            Console.WriteLine($"Response received {response}");
-            hostSocket.Shutdown(SocketShutdown.Both);
-            hostSocket.Close();
-
-            bool correctChoice = false;
-            while(correctChoice == false)
-            {
-                Console.WriteLine("You are host");
-                Console.WriteLine("Write '1'  if you want to send the message to another host ");
-
-                int decision = int.Parse(Console.ReadLine());
-                if (decision == 1)
-                {
-                    try
-                    {
-                        correctChoice = true;
-                        Console.WriteLine("Choose which host you want to send the package to");
-                        Console.WriteLine(@"To do this, write 'H1' or 'H2' or 'H3' or 'H4' -> host 1 is 'H1' etc.");
-                        string nameDestinationHost = Console.ReadLine();
-                        if (nameDestinationHost == "H1")
-                            destinationPort = h1Port;
-                        else if (nameDestinationHost == "H2")
-                            destinationPort = h2Port;
-                        else if (nameDestinationHost == "H3")
-                            destinationPort = h3Port;
-                        else if (nameDestinationHost == "H4")
-                            destinationPort = h4Port;
-
-                        Send(hostSocket, $"Test taki o se {DateTime.Now} port docelowy to {destinationPort} <EOF>"); // pomysl na wysylanie pakietu !!!!
-                        sendCompleted.WaitOne();
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e);
-                    }
-                }        
-                else
-                {
-                    correctChoice = false;
-                    Console.WriteLine("You wrote something other than '1' ");
-                    Console.WriteLine("Please try again: ");
-                }
-            }
-            
         }
 
         private static void Receive(Socket hostSocket)
@@ -197,5 +203,3 @@ namespace Host
     
 }
 
-// w jaki sposob pakujemy pakiet? - json? czy string z portem docelowym i wiadomoscia oddzielny np. srednikiem - potem cablecloud sobie odczyta to
-// SocketFlags - moze do tworzenia oakietu (rozne ifnromacje tam schowac) ????
